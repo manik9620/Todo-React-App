7095e0055fd25513ef57b463e8c8d984a106aac1
Manik <manikproch9858@gmail.com>
2024-01-30 12:25:36 +0530

    add: navbar modified

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,11 +6,11 @@ function Navbar() {
   return (
     <div className={styles.navbar}>
       <div className={styles.logoContainer}>
-        <h3>Todo App</h3>
+        <Link to="">Todo App</Link>
       </div>
         <nav >
             <ul className={styles.navbarlist}>
-                <li><Link to="">Home</Link></li>
+                {/* <li><Link to="">Home</Link></li> */}
                 <li><Link to="about">About</Link></li>
                 <li><Link to="completedtodo">Completed Todos</Link></li>
             </ul>
